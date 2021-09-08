47f6797fa43d7530f978a4622bd5c2b02c84af96
Jared Torp <jared.torp@eagles.oc.edu>
2021-09-08 12:59:58 -0500

    added all of the updated files to push to the repository

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,26 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">List</router-link>|
+      <router-link to="/add">Add</router-link>|
+      <router-link to="/adduser">Add User</router-link>|
+      <router-link to="/loginuser">Login</router-link> |
+      <router-link to="/logoutuser">Logout</router-link>
+       
     </div>
+
     <router-view />
   </div>
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
-
 #nav {
   padding: 30px;
 }
---
+++ vue.config.js
@@ -0,0 +1,10 @@
+console.log(process.env.NODE_ENV);
+module.exports = {
+  devServer: {
+    host: "localhost"
+  },
+  publicPath: process.env.NODE_ENV === "development" ? "/" : "/todoVue/"
+};
+
+
+
